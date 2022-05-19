d075bf4dcafc73fc095422fc788efd880e39fa24
Benjamin Callahan <unknown>
2022-05-19 16:43:05 +0300

    refactoring parsePgError

--- app/pkg/client/postgresql/model/error.go
+++ app/pkg/client/postgresql/model/error.go
@@ -1,6 +1,23 @@
 package model
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/jackc/pgconn"
+)
+
+// parsePgError returns parsed pgconn.PgError.
+// If err is not pgconn.PgError, returns the same err.
+func parsePgError(err error) error {
+	var pgErr *pgconn.PgError
+	if errors.Is(err, pgErr) {
+		pgErr = err.(*pgconn.PgError)
+		return fmt.Errorf("database error. message:%s, detail:%s, where:%s, sqlstate:%s",
+			pgErr.Message, pgErr.Detail, pgErr.Where, pgErr.SQLState())
+	}
+	return err
+}
 
 func ErrCommit(err error) error {
 	return fmt.Errorf("failed to commit Tx due to error: %v", err)
@@ -19,7 +36,7 @@ func ErrCreateQuery(err error) error {
 }
 
 func ErrScan(err error) error {
-	return fmt.Errorf("failed to scan due to error: %v", err)
+	return fmt.Errorf("failed to scan due to error: %v", parsePgError(err))
 }
 
 func ErrDoQuery(err error) error {
